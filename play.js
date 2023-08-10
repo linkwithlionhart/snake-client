aed50d15ccfa8fed6aa684d5ea91eefa5c8a3ebb
linkwithlionhart <linkwithlionhart@gmail.com>
2023-08-09 21:04:09 -0700

    Require setupInput module

--- play.js
+++ play.js
@@ -1,27 +1,6 @@
-const { connect } = require('./client'); 
-
-// Handle user input
-const handleUserInput = function(key) {
-  // Exit
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
-
-// Setup interface to handle user input from stdin
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  // Register event listener for stdin
-  stdin.on("data", handleUserInput)
-
-  return stdin;
-}
+const { connect } = require('./client');
+const { setupInput } = require('./input')
 
 console.log("Connecting ...");
 connect();
 setupInput();
-
