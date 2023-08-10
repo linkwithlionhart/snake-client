1d99614e740a046a8ccd24def33a805dccb09364
linkwithlionhart <linkwithlionhart@gmail.com>
2023-08-09 20:19:46 -0700

    Refactor and slight changes to code

--- play.js
+++ play.js
@@ -1,23 +1,5 @@
-const net = require("net");
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "localhost", // IP address here
-    port: 50541 // PORT number here
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8")
-
-  // event handler for incoming data
-  conn.on("data", data => {
-    console.log(data);
-  });
-
-  return conn;
-
-};
+const { connect } = require('./client'); 
 
 console.log("Connecting ...");
 connect();
