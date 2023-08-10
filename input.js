3a0e669c253e4d37fa062e074a2588e5f8336117
linkwithlionhart <linkwithlionhart@gmail.com>
2023-08-10 05:44:44 +0000

    Implement WASD movement

--- input.js
+++ input.js
@@ -1,13 +1,14 @@
-// Handle user input
-const handleUserInput = function(key) {
-  // Exit
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
+// Require modules
+const { connect } = require('./client');
+
+// Stores the active TCP connection object
+let connection;
 
 // Setup interface to handle user input from stdin
-const setupInput = function() {
+const setupInput = conn => {
+  // console.log('Setting up connection:', conn);
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -17,6 +18,27 @@ const setupInput = function() {
   stdin.on("data", handleUserInput)
 
   return stdin;
+}
+
+// Handle user input
+const handleUserInput = key => {
+  // Exit
+  if (key === '\u0003') {
+    process.exit();
+  }
+  // Up, down, left, right
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
 }
 
 module.exports = { setupInput };
