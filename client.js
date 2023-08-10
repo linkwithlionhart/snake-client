b5936bb0287b29315c5b0dc6e7785b427c29b1e9
linkwithlionhart <linkwithlionhart@gmail.com>
2023-08-09 20:19:13 -0700

    Integrate move commands

--- client.js
+++ client.js
@@ -15,6 +15,39 @@ const connect = function() {
     console.log(data);
   });
 
+  // event handler for successful connection
+  conn.on("connect", () => {
+    console.log('Successfully connected to the game server!');
+    // Send name upon connection
+    conn.write('Name: LHL');
+    // Send the "Move: up" command immediately upon connection
+    conn.write('Move: up');
+  });
+
+  /* EXPERIMENTATION
+  // What happens if we send more than one successive move message?
+  conn.on("connect", () => {
+    conn.write('Move: up');
+    conn.write('Move: up');
+    conn.write('Move: up');
+  })
+
+  // What happens if we send more than one, but delay each of them by about 50ms apart, using setTimeout
+  setTimeout(() => conn.write('Move: up'), 50);
+  setTimeout(() => conn.write('Move: up'), 100);
+  setTimeout(() => conn.write('Move: up'), 150);
+
+  // What happens if we use setInterval in order to continually move the snake up every 50ms?
+  let moveCount = 0;
+  const interval = setInterval(() => {
+    if (moveCount < 25) {
+      conn.write('Move: up');
+      moveCount++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 75); */
+
   return conn;
 
 };
